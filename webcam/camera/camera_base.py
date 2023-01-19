e2ef0094c4203b86a7cbdc146ea6444247e53400
rdejana <rdejana@me.com>
2023-01-19 16:17:48 -0700

    some clean up

--- webcam/camera/camera_base.py
+++ webcam/camera/camera_base.py
@@ -13,7 +13,7 @@ class CameraBase(traitlets.HasTraits):
         if hasattr(self, '_widget'):
             return self._widget  # cache widget, so we don't duplicate links
         from ipywidgets import Image
-        from jetbot.image import bgr8_to_jpeg
+        from webcam.image import bgr8_to_jpeg
         image = Image()
         traitlets.dlink((self, 'value'), (image, 'value'), transform=bgr8_to_jpeg)
         self._widget = image
